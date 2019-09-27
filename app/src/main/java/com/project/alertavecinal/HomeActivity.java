fdf482e94f3672cbded7510534a0bc3cb8677119
Marco Antonio Popovich <marco.popovich@globant.com>
2019-09-27 10:09:01 -0300

    fixing bug contacts fragment

--- app/src/main/java/com/project/alertavecinal/HomeActivity.java
+++ app/src/main/java/com/project/alertavecinal/HomeActivity.java
@@ -1,31 +1,29 @@
 package com.project.alertavecinal;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
-    Button btnLogout;
     FirebaseAuth mFirebaseAuth;
     FirebaseAuth.AuthStateListener firebaseAuthListener;
     private TabLayout tabLayout;
     private ViewPager viewPager;
-    private TabItem tabItem1, tabItem2, tabItem3;
     private PageAdapter pageAdapter;
 
     @Override
@@ -33,6 +31,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        initToolbar();
         mFirebaseAuth = FirebaseAuth.getInstance();
         firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
             @Override
@@ -43,7 +42,6 @@ public class HomeActivity extends AppCompatActivity {
 
         tabLayout = findViewById(R.id.tablayout);
         viewPager = findViewById(R.id.viewpager);
-        btnLogout = findViewById(R.id.logout);
 
         pageAdapter = new PageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(pageAdapter);
@@ -51,28 +49,47 @@ public class HomeActivity extends AppCompatActivity {
 
         tabLayout.setupWithViewPager(viewPager);
 
-        btnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mFirebaseAuth.signOut();
-                AuthUI.getInstance()
-                        .signOut(HomeActivity.this)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                btnLogout.setEnabled(false);
-                                Intent intToMain = new Intent(HomeActivity.this, LoginActivity.class);
-                                startActivity(intToMain);
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(HomeActivity.this,""+e.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
 
+
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.main_app_bar);
+
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_log_out_option){
+            mFirebaseAuth.signOut();
+            Intent intToMain = new Intent(HomeActivity.this, LoginActivity.class);
+            startActivity(intToMain);
+        }
+
+        if(item.getItemId() == R.id.main_find_contacts_option){
+
+        }
+
+        if(item.getItemId() == R.id.main_profile_option){
+
+        }
+
+        if(item.getItemId() == R.id.main_settings_option){
+
+        }
+
+        return true;
     }
 
     @Override
